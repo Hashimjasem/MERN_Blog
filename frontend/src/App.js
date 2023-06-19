98e66ad82043a3f3bc47009ddf00ac914d1e40ba
Hashimjasem <j_hashim1305@outlook.com>
2023-06-19 16:19:37 +0800

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <main>
+      <header>
+        <a href="" className="">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </main>
+   </div>
+  );
+}
+
+export default App;
