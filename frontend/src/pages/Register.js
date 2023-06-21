48159e9a137dce4d5fd995e9cc1011543587a6d6
Hashimjasem <j_hashim1305@outlook.com>
2023-06-21 12:31:57 +0800

    Server added

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,45 +1,65 @@
+import { useState } from "react";
+
 function Register() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function register(ev) {
+        ev.preventDefault();
+        await fetch('http://localhost4000/register', {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: {'Content-Type':'application/json'},
+        })
+    }
+
     return (
-        <div class="bg-grey-lighter min-h-screen flex flex-col">
-            <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <form class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+        <div className="bg-grey-lighter min-h-screen flex flex-col">
+            <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <form className="bg-white px-6 py-8 rounded shadow-md text-black w-full"
+                onSubmit={register}>
+
+                    <h1 className="mb-8 text-3xl text-center">Sign up</h1>
                     <input
                         type="text"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="email"
-                        placeholder="Email" />
-
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        value={email}
+                        placeholder="Email" 
+                        onChange={ev => setEmail(ev.target.value)}
+                        />
+                        
                     <input
                         type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="password"
-                        placeholder="Password" />
-                    <input
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        value={password}
+                        placeholder="Password" 
+                        onChange={ev => setPassword(ev.target.value)}
+                        />
+                    {/* <input
                         type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
                         name="confirm_password"
-                        placeholder="Confirm Password" />
+                        placeholder="Confirm Password" /> */}
 
                     <button
                         type="submit"
-                        class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                        className="w-full text-center py-3 rounded bg-green text-black hover:bg-green-dark focus:outline-none my-1"
                     >Create Account</button>
 
-                    <div class="text-center text-sm text-grey-dark mt-4">
+                    <div className="text-center text-sm text-grey-dark mt-4">
                         By signing up, you agree to the
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                        <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
                             Terms of Service
                         </a> and
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                        <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
                             Privacy Policy
                         </a>
                     </div>
                 </form>
 
-                <div class="text-grey-dark mt-6">
+                <div className="text-grey-dark mt-6">
                     Already have an account?
-                    <a class="no-underline border-b border-blue text-blue" href="../login/">
+                    <a className="no-underline border-b border-blue text-blue" href="../login/">
                         Log in
                     </a>.
                 </div>
