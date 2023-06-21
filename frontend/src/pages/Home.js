b843e0589ccd30a6cd565074759f62c5431995a1
Hashimjasem <j_hashim1305@outlook.com>
2023-06-21 11:54:25 +0800

    Login & Register UI

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,30 @@
+import BlogCard from "../components/BlogCard";
+
 const Home = () => {
-    return <h1>Home</h1>;
-  };
-  
-  export default Home;
+  return (
+    <div className="flex justify-center">
+    <div 
+    className="
+    grid
+    columns-1
+    md:columns-2 
+    mx-[1rem]
+    my-[3rem]
+    gap-y-5
+    ">
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+    </div>
+    </div>
+  )
+};
+
+export default Home;
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,46 @@
 function Login(
 ) {
-    return ( 
-        <>
-        login</>
-     );
+    return (
+        <div class="bg-grey-lighter min-h-screen flex flex-col">
+            <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <form class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <h1 class="mb-8 text-3xl text-center">Login</h1>
+                    <input
+                        type="text"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email" />
+
+                    <input
+                        type="password"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" />
+                    <button
+                        type="submit"
+                        class="w-full text-center py-3 rounded bg-green text-Black hover:bg-green-dark focus:outline-none my-1"
+                    >Login</button>
+
+                    <div class="text-center text-sm text-grey-dark mt-4">
+                        By signing up, you agree to the
+                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                            Terms of Service
+                        </a> and
+                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                            Privacy Policy
+                        </a>
+                    </div>
+                </form>
+
+                <div class="text-grey-dark mt-6">
+                    Don't have an account?
+                    <a class="no-underline border-b border-blue text-blue" href="../register/">
+                        Register
+                    </a>.
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
