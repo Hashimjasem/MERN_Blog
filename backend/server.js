fc7ccee5fe2b7448dc3e17d7d126c857975e7cbd
Hashimjasem <j_hashim1305@outlook.com>
2023-06-22 13:14:44 +0800

    bcrypt and JWT

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,44 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 const app = express()
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10)
+const User = require('./models/User.js')
+const jwt = require('jsonwebtoken')
+const secret = '932hdj8347d9002mshdh93'
 
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(express.json())
 
-app.post('/register', (req,res) => {
-    res.json('test okay');
+mongoose.connect('mongodb+srv://jhashim1305:JASEM.h02@blogcluster.rqxxm5q.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt)
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(400).json(e)
+    }
 });
 
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+        const userDoc = await User.findOne({username});
+        const passOk = bcrypt.compareSync(password, userDoc.password)
+        if (passOk) {
+            //login
+            jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json('ok')
+            });
+        } else {
+            res.status(400).json('wrong credentials');
+        }
+});
 app.listen(4000)
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,40 +1,67 @@
-function Login(
-) {
+import { useState } from "react";
+import { Navigate} from "react-router-dom";
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false)
+
+    async function login(ev) {
+        ev.preventDefault();
+        const response = await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include',
+        });
+        if (response.ok) {
+            setRedirect(true)
+        } else {
+            alert('wrong credentials')
+        }
+    }
+
+    if (redirect) {
+        return <Navigate to={'/'}/>
+    }
     return (
-        <div class="bg-grey-lighter min-h-screen flex flex-col">
-            <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <form class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    <h1 class="mb-8 text-3xl text-center">Login</h1>
+        <div className="bg-grey-lighter min-h-screen flex flex-col">
+            <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <form 
+                className="bg-white px-6 py-8 rounded shadow-md text-black w-full"
+                onSubmit={login}>
+                    <h1 className="mb-8 text-3xl text-center">Login</h1>
                     <input
                         type="text"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="email"
-                        placeholder="Email" />
-
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        value={username}
+                        placeholder="Username" 
+                        onChange={ev => setUsername(ev.target.value)}/>
                     <input
                         type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="password"
-                        placeholder="Password" />
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        value={password}
+                        placeholder="Password"
+                        onChange={ev => setPassword(ev.target.value)} />
                     <button
                         type="submit"
-                        class="w-full text-center py-3 rounded bg-green text-Black hover:bg-green-dark focus:outline-none my-1"
+                        className="w-full text-center py-3 rounded bg-green text-Black hover:bg-green-dark focus:outline-none my-1"
                     >Login</button>
 
-                    <div class="text-center text-sm text-grey-dark mt-4">
+                    <div className="text-center text-sm text-grey-dark mt-4">
                         By signing up, you agree to the
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                        <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
                             Terms of Service
                         </a> and
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                        <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
                             Privacy Policy
                         </a>
                     </div>
                 </form>
 
-                <div class="text-grey-dark mt-6">
+                <div className="text-grey-dark mt-6">
                     Don't have an account?
-                    <a class="no-underline border-b border-blue text-blue" href="../register/">
+                    <a className="no-underline border-b border-blue text-blue" href="../register/">
                         Register
                     </a>.
                 </div>
