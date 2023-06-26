f8d6beb2802220e0807a8b4b0c82aecfab7164c7
Hashimjasem <j_hashim1305@outlook.com>
2023-06-26 20:45:47 +0800

    Set Cover Image As Background

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -4,12 +4,10 @@ import { Link } from "react-router-dom";
 export default function Post({ _id, title, summary, cover, content, createdAt, author }) {
 
   return (
-    <div className="post">
-      <div className="image">
-        <Link to={`/post/${_id}`}>
-          <img src={'http://localhost:4000/' + cover} alt="" />
-        </Link>
-      </div>
+    <div
+      className="h-[30rem] w-[80%] mx-auto my-[5rem] bg-cover"
+      style={{ backgroundImage: `url(${'http://localhost:4000/' + cover})` }}>
+
       <div className="texts">
         <Link to={`/post/${_id}`}>
           <h2>{title}</h2>
